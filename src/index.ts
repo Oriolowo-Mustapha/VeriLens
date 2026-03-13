a6d7e6d470ea2c366b3b0de168e357c757888796
Oriolowo-Mustapha <oriolowomustapha@gmail.com>
2026-03-13 08:33:55 +0100

    Configure CORS to allow specific origins and enable credentials

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,14 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://127.0.0.1:5500',
+    'http://localhost:5500',
+    'https://verilens-frontend.pxxl.click'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use((req, res, next) => {
